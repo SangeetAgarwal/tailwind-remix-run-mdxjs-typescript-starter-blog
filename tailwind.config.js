e2dc578e72ce119c686dd165b9b38fb6adc670bf
Sangeet Agarwal <sangeet.m.agarwal@gmail.com>
2023-06-10 22:51:49 -0400

    style: default font is Proxima nova

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
         14: "3.5rem",
       },
       fontFamily: {
-        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+        sans: ["Proxima Nova", ...defaultTheme.fontFamily.sans],
       },
       colors: {
         primary: colors.teal,
